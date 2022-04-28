33e7a206f63622c9883412ff320e3aac8865601b
Stuart Heath <heaths8@uni.coventry.ac.uk>
2022-04-28 13:19:56 +0100

    SH Cover

--- Assets/Stuart/Scripts/NeonChangeOverTime.cs
+++ Assets/Stuart/Scripts/NeonChangeOverTime.cs
@@ -1,31 +1,42 @@
 using System;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.PostProcessing;
 
 namespace Stuart.Scripts
 {
     public class NeonChangeOverTime : MonoBehaviour
     {
         private bool goingUp;
-        [SerializeField] [Range(0.5f,4f)] private float neonIntensityLow=1f;
-        [SerializeField] [Range(0.5f,4f)] private float neonIntensityHigh = 2f;
+        [SerializeField] [Range(0.5f,4f)] private float bloomIntensityLow=3f;
+        [SerializeField] [Range(0.5f,4f)] private float bloomIntensityHigh = 4f;
         [SerializeField] private float changeSpeed = 2f;
-        [SerializeField] private Material neonMaterial;
-
-        [SerializeField] LevelAesteticData levelAesteticData;
-        /*private void Update()
+        [SerializeField] Bloom bloom;
+      
+        private void Update()
         {
-            if (levelAesteticData.neonIntensity<=neonIntensityHigh)
+            ChangeBloomIntensitySettings();
+        }
+        private void ChangeBloomIntensitySettings()
+        {
+           
+            
+            //var postProcessVolume = FindObjectOfType<PostProcessVolume>();
+            var postProcessVolume = GetComponent<Volume>();
+            if (postProcessVolume == null)
             {
-                levelAesteticData.neonIntensity =
-                    Mathf.Lerp(neonIntensityLow, neonIntensityHigh, changeSpeed * Time.deltaTime);
+                Debug.LogWarning("No post processing");
+                return;
             }
-            else
-            {
-                levelAesteticData.neonIntensity =
-                    Mathf.Lerp(neonIntensityHigh, neonIntensityLow, changeSpeed * Time.deltaTime);
-            }
-         
-        }*/
+            Bloom bloom = postProcessVolume.profile.GetSetting<Bloom>();
+
+            var intensity = new UnityEngine.Rendering.PostProcessing.FloatParameter();
+            intensity.value = bloomIntensityLow+ Mathf.PingPong(Time.time*changeSpeed, bloomIntensityHigh-bloomIntensityLow);
+
+            bloom.intensity = intensity;
+            bloom.intensity.value = intensity.value;
+            bloom.enabled.value = true;   
+        }
     }
 }
