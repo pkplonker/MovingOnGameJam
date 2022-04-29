8c66e0e5d93df91135de25a27319478e01b7ecde
NathanCoxson <ncoxson.business@gmail.com>
2022-04-29 16:11:50 +0100

    Rat Level

--- Assets/Stuart/Scripts/SO/Character/CombatStats.cs
+++ Assets/Stuart/Scripts/SO/Character/CombatStats.cs
@@ -5,7 +5,7 @@ namespace Stuart.Scripts.SO.Character
 	[CreateAssetMenu(fileName = "Combat", menuName = "Character/Stats/Combat")]
 	public class CombatStats : ScriptableObject
 	{
-		[Range(1, 15)] public float attackRadius = 2f;
+		public float attackRadius = 2f;
 		[Range(0, 10)] public float attackSpeed = 2f;
 		public ProjectileData projectileData;
 	}
